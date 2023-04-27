12dbbc907e66e902e5690eeaf66675ce1ffe1641
yeo sugyeong <ssyeo1204@gmail.com>
2023-04-28 00:47:01 +0900

    gameover 화면까지 제작

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ canvas.height = 600;
 document.body.appendChild(canvas);
 
 //// 이미지 로드!
-let backgroundImage, chimImage, enemyImage, gameoverImage;
+let backgroundImage, chimImage, enemyImage, gameOverImage;
+let gameOver = false // true이면 게임이 끝, false면 게임 유지
+let score = 0
 
 //// 침착맨 좌표!
 // chim Image 64*90
@@ -25,8 +27,8 @@ function loadImage() {
   enemyImage = new Image();
   enemyImage.src = 'img/enemy.png'
 
-  gameoverImage = new Image();
-  gameoverImage.src = 'img/gameover.jpg'
+  gameOverImage = new Image();
+  gameOverImage.src = 'img/gameover.png'
 }
 
 //// 방향키 세팅!
@@ -58,22 +60,91 @@ function update() {
   if(chimX >= canvas.width - 64) {
     chimX = canvas.width - 64
   }
+
+  // 적군 y값 업데이트
+  for(let i = 0; i < enemyList.length; i++){
+    enemyList[i].update()
+  }
+
+}
+
+//// 랜덤
+function generateRandomValue(min, max) {
+  let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+  // console.log(randomNum)
+  return randomNum
+}
+
+//// 침착맨
+// function chim () {
+//   this.touch = function() {
+//     for(let i = 0; i < enemyList.length; i++){
+//       if(this.x <= enemyList[i].x+64 && this.x >= enemyList[i].x){
+//         // 쭈를 피하면 점수 획득
+//         score++;
+        
+//       }
+//       console.log(enemyList.length)
+//     }
+//   }
+// }
+
+
+//// 적군
+let enemyList = []
+function Enemy() {
+  this.x = 0;
+  this.y = 0;
+  this.init = function() {
+    this.y = 0
+    this.x = generateRandomValue(0 , canvas.width - 64)
+    enemyList.push(this)
+  }
+  this.update = function() {
+    this.y += 3 // 적군 떨어지는 속도 조절
+
+    if(this.y >= canvas.height - 74){
+      gameOver = true;
+      console.log('gameOver')
+    }
+  }
+
+
+}
+
+// 적군 생성
+function createEnemy() {
+  const interval = setInterval(function(){
+    let e = new Enemy()
+    e.init()
+  } , 700)
 }
 
 //// UI 그리기!
 function render() {
   ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
   ctx.drawImage(chimImage, chimX, chimY)
+
+  // 적군
+  for(let i = 0; i < enemyList.length; i++) {
+    // console.log(enemyList.length)
+    ctx.drawImage(enemyImage, enemyList[i].x, enemyList[i].y)
+  }
 }
 
 //// gif를 계속해서 호출하기!
 function main() {
-  update() // 좌표값 업데이트
-  render() // 배경 이미지
-  requestAnimationFrame(main)
+  if(!gameOver){
+    update() // 좌표값 업데이트
+    render() // 배경 이미지
+    requestAnimationFrame(main)
+  } else{
+    ctx.drawImage(gameOverImage,0,0)
+  }
 }
 
 //// 호출
 loadImage()
 setupKeyboardListener()
+createEnemy()
 main()
