443c346b28886076121ab4d66628d8b09e6d7f07
yeo sugyeong <ssyeo1204@gmail.com>
2023-04-30 21:22:09 +0900

    스코어 1점씩 추가되기

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let gameOver = false // true이면 게임이 끝, false면 게임 유지
 let score = 0
 
 //// 침착맨 좌표!
-// chim Image 64*90
+//// chim Image 64*90
 let chimX = canvas.width / 2 - 32;
 let chimY = canvas.height - 100;
 
@@ -65,32 +65,15 @@ function update() {
   for(let i = 0; i < enemyList.length; i++){
     enemyList[i].update()
   }
-
 }
 
-//// 랜덤
+//// (적군) 랜덤!
 function generateRandomValue(min, max) {
   let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
-  // console.log(randomNum)
   return randomNum
 }
 
-//// 침착맨
-// function chim () {
-//   this.touch = function() {
-//     for(let i = 0; i < enemyList.length; i++){
-//       if(this.x <= enemyList[i].x+64 && this.x >= enemyList[i].x){
-//         // 쭈를 피하면 점수 획득
-//         score++;
-        
-//       }
-//       console.log(enemyList.length)
-//     }
-//   }
-// }
-
-
-//// 적군
+//// 적군!
 let enemyList = []
 function Enemy() {
   this.x = 0;
@@ -99,16 +82,12 @@ function Enemy() {
     this.y = 0
     this.x = generateRandomValue(0 , canvas.width - 64)
     enemyList.push(this)
+    avoidEnemy()
+    meetEnemy()
   }
   this.update = function() {
     this.y += 3 // 적군 떨어지는 속도 조절
-
-    if(this.y >= canvas.height - 74){
-      gameOver = true;
-      console.log('gameOver')
-    }
   }
-
 
 }
 
@@ -117,17 +96,48 @@ function createEnemy() {
   const interval = setInterval(function(){
     let e = new Enemy()
     e.init()
-  } , 700)
+    if(gameOver){ // 게임오버일때 적군 생성 멈추기
+      clearInterval(interval)
+    }
+  } , 800)
+}
+
+
+// 적군을 피하면 스코어 1점 얻기
+function avoidEnemy(){
+  for(let i = 0; i < enemyList.length; i++) {
+    if(enemyList[i].y > canvas.height - 100 ){
+      score++
+      enemyList.splice(i, 1) // 마지막 배열 값 삭제
+      i--;
+    }
+  }
+}
+
+// 적군을 만나면 게임오버
+function meetEnemy() {
+  for(let i = 0; i < enemyList.length; i++){
+    if(enemyList[i].y >= chimY - 68
+      && enemyList[i].x <= chimX
+      && enemyList[i].x + 64 >= chimX
+      ){
+      gameOver = true;
+      console.log('gameover')
+    }
+    console.log('chimY', chimY)
+    console.log('enemyList[i].y', enemyList[i].y)
+  }
 }
 
 //// UI 그리기!
 function render() {
   ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
-  ctx.drawImage(chimImage, chimX, chimY)
+  ctx.drawImage(chimImage, chimX, chimY);
+  ctx.fillText(`Score: ${score}`, 20, 30);
+  ctx.fillStyle = 'white';
+  ctx.font = '20px Arial';
 
-  // 적군
   for(let i = 0; i < enemyList.length; i++) {
-    // console.log(enemyList.length)
     ctx.drawImage(enemyImage, enemyList[i].x, enemyList[i].y)
   }
 }
@@ -139,7 +149,7 @@ function main() {
     render() // 배경 이미지
     requestAnimationFrame(main)
   } else{
-    ctx.drawImage(gameOverImage,0,0)
+    ctx.drawImage(gameOverImage, 0, 0)
   }
 }
 
